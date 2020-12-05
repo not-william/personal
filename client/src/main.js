35b48cef128c7b1567b33928038fa83c4e35ab79
William Harris <william@hgat.co.uk>
2020-12-05 20:08:51 +0000

    Change api url

--- client/src/main.js
+++ client/src/main.js
@@ -8,7 +8,7 @@ import '@/assets/styles/app.css'
 
 require('@/store/subscriber.js')
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api/'
+axios.defaults.baseURL = 'http://143.110.170.163:8000/api/'
 
 Vue.config.productionTip = false
 
